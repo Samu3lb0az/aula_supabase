2379c75250498d380a07e5b6cb3af57cfa16a78d
Samu3lb0az <samuelboaz6@gmail.com>
2025-05-15 12:21:52 -0300

    commit2

--- supabaseConfig.js
+++ supabaseConfig.js
@@ -1,10 +1,13 @@
-import { createClient } from "@supabase/supabase-js"; 
+import 'react-native-url-polyfill/auto';    
+import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = "https://vnyrejkdgxhddfshqwxj.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZueXJlamtkZ3hoZGRmc2hxd3hqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczMDc5MDEsImV4cCI6MjA2Mjg4MzkwMX0.RLN9lSM2qtVDmmnAihyWYaKCbWiIfUWBHLU6ywT82w8";
+const SUPABASE_URL = 'https://vnyrejkdgxhddfshqwxj.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZueXJlamtkZ3hoZGRmc2hxd3hqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczMDc5MDEsImV4cCI6MjA2Mjg4MzkwMX0.RLN9lSM2qtVDmmnAihyWYaKCbWiIfUWBHLU6ywT82w8';
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
-  throw new Error("Supabase  URL ou chave não estão configuradas corretamente.");
+  throw new Error('Supabase URL ou anon key não configuradas.');
 }
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+export default supabase;
